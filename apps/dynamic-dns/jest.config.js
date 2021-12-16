df4198589fa85ae8ed1f3c35386d03cdfa40acfb
Matthew Zhao <matthew@fusebit.io>
2021-12-15 20:19:49 -0800

    .

---
+++ apps/dynamic-dns/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'dynamic-dns',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/dynamic-dns',
+};
